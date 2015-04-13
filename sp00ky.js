4857e806d3c057a43b0d563e4ec00b69b8972a42
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2015-04-13 13:19:50 +0200

    fixes

--- sp00ky.js
+++ sp00ky.js
@@ -1,4 +1,5 @@
-//spookyPics 
+//spookyPics
+//U'LL GET 2 SP00K3D
 (function ($) {
 
     var self = {
@@ -9,7 +10,10 @@
     "http://www.tncgiftsncollectibles.com/1789-1028-thickbox/spooky-skeleton-toilet-paper-holder-12608.jpg",
     "http://i.ytimg.com/vi/101jYcZQ8O8/maxresdefault.jpg",
     "https://mikedoel.files.wordpress.com/2008/10/skeleton11.jpg",
-    "http://static.tumblr.com/85b2e40e1c4a4df343f51a6f184e6a0e/lntarg1/35kmt3fni/tumblr_static_dancing_tophat_skeleton_t.png"
+    "http://static.tumblr.com/85b2e40e1c4a4df343f51a6f184e6a0e/lntarg1/35kmt3fni/tumblr_static_dancing_tophat_skeleton_t.png",
+    "http://img3.wikia.nocookie.net/__cb20140924174011/trollpasta/images/7/79/HS-H-PAPERSKLTN.jpg",
+    "http://media2.giphy.com/media/oLyIz3quUlD1e/200_s.gif"
+
 
         ],
         handleImages: function (lstImgs, time) {
